e676454b462b8e3e8271206b9719c876816b444c
Alejandro Casanova <36633619+Alejandro-Casanova@users.noreply.github.com>
2025-11-19 17:39:38 +0100

    feat: add command-line arguments for logging configuration

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import tkinter as tk
 
 from core.app import App
 from core.settings.logger import init_logger_minimal
+import argparse
 
 def main():
-    init_logger_minimal(verbose=False, color=True)
-    
+
+    parser = argparse.ArgumentParser(description='Enhanced Image Converter')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose logging')
+    parser.add_argument('--no-color', action='store_true', help='Disable colored output')
+    args = parser.parse_args()
+
+    init_logger_minimal(verbose=args.verbose, color=not args.no_color)
+
     root: tk.Tk = tk.Tk()
     app = App(root)
 
